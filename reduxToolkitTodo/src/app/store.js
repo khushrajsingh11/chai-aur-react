975a4b603dd607fbb435564aceb34dc1a1a7abad
khushrajsingh11 <137679795+khushrajsingh11@users.noreply.github.com>
2024-12-10 11:25:06 +0530

    Update store.js

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -2,5 +2,6 @@ import {configureStore} from '@reduxjs/toolkit';
 import todoReducer from '../features/todo/todoSlice';
 
 export const store = configureStore({
-    reducer: todoReducer
+    reducer:{ todoReducer
+            }
 })
